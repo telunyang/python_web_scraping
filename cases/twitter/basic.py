5ee12d16cc2aa3eb2cdea999d68c55af5cf89c8f
Darren Yang <telunyang@gmail>
2022-06-20 02:13:21 +0800

    新增範例

---
+++ cases/twitter/basic.py
@@ -0,0 +1,26 @@
+'''
+匯入套件
+'''
+import twint
+import logging, time
+import pandas as pd
+from time import sleep, time
+import nest_asyncio
+nest_asyncio.apply()
+
+# twint 設定，可參考 https://github.com/twintproject/twint/blob/master/twint/config.py
+config = twint.Config()
+
+# twint 設定初始化
+config.Lang = 'zh-tw' # 語系: https://developer.twitter.com/en/docs/twitter-for-websites/supported-languages
+config.Search = '"機器學習" OR "自然語言處理"' # 搜尋關鍵字
+config.Limit = 1000000000000 # 限定抓多少筆資料
+config.Since = '2022-06-01 00:00:00' # 設定 since 與 until
+config.Until = '2022-06-19 23:59:59' # since 與 until 記得至少相差 1 天
+# config.Geo = "48.880048,2.385939,5km"
+config.Hide_output = False
+config.Store_csv = True
+config.Output = f"./twitter.csv"
+
+# 進行資料抓取
+twint.run.Search(config)
--- cases/twitter/twint_run.py
+++ cases/twitter/twint_run.py
@@ -2,38 +2,26 @@
 匯入套件
 '''
 import twint
-import logging, time
-from time import sleep, time
+import time
+from time import time
 import nest_asyncio
 nest_asyncio.apply()
 
-'''
-全域設定
-'''
-# logging 設定
-logging.basicConfig(
-    filename = 'twitter.log',
-    filemode = 'a',
-    format = '%(asctime)s - %(levelname)s - %(message)s',
-    datefmt = '%Y-%m-%d %H:%M:%S',
-    encoding = "utf-8",
-    level = logging.INFO
-)
 
 '''
 自訂函式
 '''
 # 取得 tweet 資料
-def get_tweets():
-    # twint 設定，可參考 https://github.com/twintproject/twint/blob/master/twint/config.py
+def get_tweets(since, until):
+    # twint 設定
     config = twint.Config()
 
     # twint 設定初始化
-    config.Lang = 'zh-tw' # 語系: https://developer.twitter.com/en/docs/twitter-for-websites/supported-languages
-    config.Search = '"機器學習" OR "自然語言處理"' # 搜尋關鍵字
-    config.Limit = 1000000000000 # 限定抓多少筆資料
-    config.Since = '2019-10-01 00:00:00' # 設定 since 與 until
-    config.Until = '2020-12-31 23:59:59' # since 與 until 記得至少相差 1 天
+    config.Lang = 'zh-tw'
+    config.Search = '"機器學習" OR "自然語言處理"' 
+    config.Limit = 1000000000000
+    config.Since = since
+    config.Until = until
     # config.Geo = "48.880048,2.385939,5km"
     config.Hide_output = False
     config.Store_csv = True
@@ -44,18 +32,13 @@ def get_tweets():
 
 # 主要執行程式
 def main():
+    since = '2022-06-01 00:00:00'
+    until = '2022-06-19 23:59:59'
+
     try:   
-        # 重複執行
-        while True:
-            logging.info(f"程式準備啟動...")
-            t_sec = time() # 計算時間
-            get_tweets()
-            logging.info(f"程式執行結束，本次花費時間: {time() - t_sec} 秒。")
+        get_tweets(since, until)
     except Exception as ex:
-        # 休息一段時間後，重新執行程式
-        logging.info(f"程式拋出例範外，已中斷...暫停 1800 秒後重新執行...")
-        sleep(1800)
-        main() # 再執行一次主程式
+        print(ex)
 
 '''
 主程式區域
@@ -68,4 +51,4 @@ if __name__ == "__main__":
     main()
 
     # 完整執行花費時間
-    logging.info(f"完整執行花費時間: {time() - t_sec} 秒。")
+    print(f"完整執行花費時間: {time() - t_sec} 秒。")
